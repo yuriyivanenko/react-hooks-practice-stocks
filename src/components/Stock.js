69a8f251bb915803b4981da544ba8000e9c2e116
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-24 09:53:13 -0400

    Fetch and render all stocks from db.json

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react'
 
-function Stock() {
+function Stock({ stockInfo: { ticker, name, type, price } }) {
   return (
     <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title'>{name}</h5>
+          <p className='card-text'>{`${ticker}: ${price}`}</p>
         </div>
       </div>
     </div>
-  );
+  )
 }
-export default Stock;
+export default Stock
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,24 @@
-import React from "react";
-import Stock from "./Stock";
+import React, { useEffect, useState } from 'react'
+import Stock from './Stock'
 
 function StockContainer() {
+  const [stockList, setStockList] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+      .then((res) => res.json())
+      .then((data) => setStockList(data))
+  }, [])
+
+  if (!stockList) return <h1>Loading...</h1>
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockList.map((stock) => {
+        return <Stock key={`stock-${stock.id}`} stockInfo={stock} />
+      })}
     </div>
-  );
+  )
 }
 
-export default StockContainer;
+export default StockContainer
